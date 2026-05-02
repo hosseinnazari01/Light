cefbd6577844e648081f4a4d6451c73337664bac
Hossein Nazari/ <hossein.nazari.computer01@gmail.com>
2026-05-02 23:29:40 +0330

    Create Light.py

---
+++ Light/Light.py
@@ -0,0 +1,9 @@
+color=input("Enter a Color (Red - Yellow - Green): ")
+if color == "Red" or color == "Red":
+    print("STOP!")
+elif color == "Yellow" or color == "Yellow":
+    print("Warning!")
+elif color == "Green" or color == "Green":
+    print("Go")
+else:
+    print("Error")
